7163432f5ae525112809990d8fad1c4d22dbe0ed
monturet-matthieu <matthieu.monturet@edu.univ-fcomte.fr>
2023-04-16 14:59:34 +0200

    correction bug netlify

--- src/router/index.js
+++ src/router/index.js
@@ -7,12 +7,6 @@ import VelotafView from '../views/Reals/VelotafView.vue'
 import FemininView from '../views/Reals/FemininView.vue'
 import JPOMMIView from '../views/Reals/JPOMMIView.vue'
 import TikTakView from '../views/Reals/TikTakView.vue'
-import BlueJazzView from '../views/Reals/BlueJazzView.vue'
-import FildeferView from '../views/Reals/FildeferView.vue'
-import AquamanView from '../views/Reals/AquamanView.vue'
-import CGCracView from '../views/Reals/CGCracView.vue'
-import CitadelleView from '../views/Reals/CitadelleView.vue'
-import ExpoView from '../views/Reals/ExpoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,12 +19,6 @@ const router = createRouter({
     { path: '/reals/feminin',   name: 'feminin',    component: FemininView    },
     { path: '/reals/jpommi',    name: 'jpommi',     component: JPOMMIView     },
     { path: '/reals/tiktak',    name: 'tiktak',     component: TikTakView     },
-    { path: '/reals/bluejazz',  name: 'bluejazz',   component: BlueJazzView   },
-    { path: '/reals/fildefer',  name: 'fildefer',   component: FildeferView   },
-    { path: '/reals/aquaman',   name: 'aquaman',    component: AquamanView    },
-    { path: '/reals/19Crac',    name: '19Crac',     component: CGCracView     },
-    { path: '/reals/citadelle', name: 'citadelle',  component: CitadelleView  },
-    { path: '/reals/expo',      name: 'expo',       component: ExpoView       },
   ]
 })
 
