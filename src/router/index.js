b4e54bb26cb4bd1de2601b3fc3b1fff0e0501f7e
monturet-matthieu <matthieu.monturet@edu.univ-fcomte.fr>
2023-09-21 13:17:25 +0200

    reroute views

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,10 @@ import HomeView from '../views/HomeView.vue'
 import RealsView from '../views/RealsView.vue'
 import ContactView from '../views/ContactView.vue'
 import AboutView from '../views/AboutView.vue'
-import VelotafView from '../views/Reals/VelotafView.vue'
-import FemininView from '../views/Reals/FemininView.vue'
-import JPOMMIView from '../views/Reals/JPOMMIView.vue'
-import TikTakView from '../views/Reals/TikTakView.vue'
+import VelotafView from '../views/VelotafView.vue'
+import FemininView from '../views/FemininView.vue'
+import JPOMMIView from '../views/JPOMMIView.vue'
+import TikTakView from '../views/TikTakView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,10 +15,10 @@ const router = createRouter({
     { path: '/reals',           name: 'reals',      component: RealsView      },
     { path: '/contact',         name: 'contact',    component: ContactView    },
     { path: '/about',           name: 'about',      component: AboutView      },
-    { path: '/reals/velotaf',   name: 'velotaf',    component: VelotafView    },
-    { path: '/reals/feminin',   name: 'feminin',    component: FemininView    },
-    { path: '/reals/jpommi',    name: 'jpommi',     component: JPOMMIView     },
-    { path: '/reals/tiktak',    name: 'tiktak',     component: TikTakView     },
+    { path: '/velotaf',         name: 'velotaf',    component: VelotafView    },
+    { path: '/feminin',         name: 'feminin',    component: FemininView    },
+    { path: '/jpommi',          name: 'jpommi',     component: JPOMMIView     },
+    { path: '/tiktak',          name: 'tiktak',     component: TikTakView     },
   ]
 })
 
