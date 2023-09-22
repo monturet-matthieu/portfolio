bddb66a71453be39b71382666e4f0fe14d95e587
monturet-matthieu <matthieu.monturet@edu.univ-fcomte.fr>
2023-09-22 16:07:25 +0200

    Ajout page JHM

---
+++ src/views/JHMView.vue
@@ -0,0 +1,166 @@
+<template>
+    <!--Réseaux-->
+    <nav class="fixed">
+        <div class="flex flex-col gap-5 bg-blanc-gris px-2 py-4 desktop:py-5 desktop:px-4 rounded-r-xl desktop:rounded-r-3xl">
+            <a href="https://twitter.com/MatthieuMontur1" target="_blank">
+                <img class="w-6 desktop:w-12" src="@/assets/icons/twitter-icon.svg" alt="twitter icon">
+            </a>
+            <a href="https://github.com/monturet-matthieu">
+                <img class="w-6 desktop:w-12" src="@/assets/icons/github-icon.svg" alt="github icon">
+            </a>
+            <a href="https://www.linkedin.com/in/matthieumonturet" target="_blank">
+                <img class="w-6 desktop:w-12" src="@/assets/icons/linkedin-icon.svg" alt="linkedin icon">
+            </a>
+        </div>
+    </nav>
+    <!--Corps de la page-->
+    <h2 class="text-blanc-gris font-gothic text-center font-semibold text-3xl desktop:text-6xl my-12">JHM CONCEPT</h2>
+    <div class="text-blanc-gris font-karla text-lg desktop:text-xl mx-10">
+        <!--Présentation-->
+        <div class="mb-8 desktop:mx-[428px]">
+            <h3 class="font-gothic font-medium text-xl desktop:text-3xl my-6">Présentation</h3>
+            <p>JHM Concept est l'entreprise dans laquelle j'ai pu effectuer mon stage de 2e année lors de mon BUT MMI. Durant ce stage de 10 semaines, j'ai été chargé de plusieurs missions, dont la principale était de développer le site web de l'entreprise en utilisant la technologie WordPress et à l'aide du thème Avada. J'ai donc dû réaliser toutes les étapes de la réalisation du site, de sa contextualisation jusqu'à sa mise en ligne.</p>
+        </div>
+        <!--Site web-->
+        <div class="mb-8 desktop:mx-[428px]">
+            <h3 class="font-gothic font-medium text-xl desktop:text-3xl my-6">Site web</h3>
+            <div class="flex flex-col items-center">
+                <img class="my-6 w-full desktop:w-[1000px]" src="/img/jhm/jhm-accueil.png" alt="site web JHM CONCEPT">
+                <a href="https://jhm-concept.com" target="_blank">
+                    <button class="font-cabin rounded-full shadow shadow-black/60 bg-orange-500 py-2 px-6 desktop:py-4 desktop:px-12 uppercase text-lg desktop:text-2xl font-black mb-8">
+                        Visiter le site
+                    </button>
+                </a>
+            </div>
+            <p>Le site JHM CONCEPT est composé de deux catégories de pages, complétées par une en-tête donnant accès à toutes les pages du site, une section Contact en bas de chaque page du site (en dehors de la page d'accueil) et un pied de page contenant les pages les plus importantes. Malheureusement, le site n'est pas totalement complet dû au manque de contenus textuels et imagés, qui devaient m'être fournis par les membres de l'équipe de l'entreprise. Certaines pages ne contiennent donc pas encore les informations qu'elles ont pour but de présenter.</p>
+            <div class="flex flex-col items-center gap-5 my-8 italic text-base desktop:text-lg text-center">
+                <img class="w-full desktop:w-[1000px]" src="/img/jhm/jhm-header.png" alt="Header site JHM CONCEPT">
+                <caption>Header du site</caption>
+                <img class="w-full mt-2 desktop:w-[1000px]" src="/img/jhm/jhm-sectioncontact.png" alt="Section Contact site JHM CONCEPT">
+                <caption>Section Contact en fin de page</caption>
+                <img class="w-full mt-2 desktop:w-[1000px]" src="/img/jhm/jhm-footer.png" alt="Footer site JHM CONCEPT">
+                <caption>Footer du site</caption>
+            </div>
+            <p>La page d'accueil opte pour une présentation graphique des différentes pages du site. La disposition organique des éléments sur la page a été pensée pour mettre en avant la liaison entre toutes les compétences de l'entreprise, tout en les mettant individuellement en lumière grâce aux animations interactives lors de la navigation sur la page.</p>
+            <div class="flex flex-col items-center gap-5 mb-8 italic desktop:text-lg text-base">
+                <img class="mt-6 w-full desktop:w-[1000px]" src="/img/jhm/jhm-accueil.png" alt="Page d'accueil du site JHM CONCEPT">
+                <caption>Page d'accueil du site</caption>
+            </div>
+            <p>La première catégorie de pages est la catégorie "classique", à savoir les pages que l'on retrouve sur la majorité des sites web d'entreprise : une page de présentation de l'entreprise, de ses réalisations et une page de contact.</p>
+            <div class="flex flex-col items-center gap-5 mb-8 italic desktop:text-lg text-base">
+                <img class="mt-6 w-full desktop:w-[1000px]" src="/img/jhm/jhm-pres.png" alt="Page de présentation de l'entreprise">
+                <caption>Aperçu de la page de présentation de l'entreprise</caption>
+            </div>
+            <div class="flex flex-col items-center gap-5 mb-8 italic desktop:text-lg text-base">
+                <img class="mt-6 w-full desktop:w-[1000px]" src="/img/jhm/jhm-reals.png" alt="Page de présentation de l'entreprise">
+                <caption>Aperçu de la page de présentation des réalisations de l'entreprise</caption>
+            </div>
+            <div class="flex flex-col items-center gap-5 mb-8 italic desktop:text-lg text-base">
+                <img class="mt-6 w-full desktop:w-[1000px]" src="/img/jhm/jhm-contact.png" alt="Page de présentation de l'entreprise">
+                <caption>Aperçu de la page de contact du site</caption>
+            </div>
+            <p>La deuxième catégorie met en avant les compétences de l'entreprise, en allant des capacités globales jusqu'à détailler chaque processus de réalisation et/ou fabrication d'une prestation.</p>
+            <p>La page Nos compétences présente les domaines dans lesquels l'entreprise propose ses services, en redirigeant vers ces derniers.</p>
+            <div class="flex flex-col items-center gap-5 mb-8 italic desktop:text-lg text-base">
+                <img class="mt-6 w-full desktop:w-[1000px]" src="/img/jhm/jhm-comp.png" alt="Page de présentation de l'entreprise">
+                <caption>Aperçu de la page de présentation des domaines d'activité de l'entreprise</caption>
+            </div>
+            <p>Ces pages à la structure similaire présentent chacune un domaine, en avançant les différentes prestations que JHM CONCEPT peut réaliser. Au total, JHM propose 11 prestations distinctes dans 3 domaines d'activités différents : Bureau d'études, Production et Métrologie.</p>
+            <p class="mt-4">Les prestations relevant du domaine Bureau d'études sont toutes liées à l'étude de projet, comme par exemple la réalisation de dossiers techniques et la réalisation de calculs déterminant les modalités de production en amont de cette dernière.</p>
+            <div class="flex flex-col items-center gap-5 mb-8 italic desktop:text-lg text-base">
+                <img class="mt-6 w-full desktop:w-[1000px]" src="/img/jhm/jhm-bureau.png" alt="Page de présentation de l'entreprise">
+                <caption>Aperçu de la page de présentation des prestations du domaine "Bureau d'études".</caption>
+            </div>
+            <p>Les pages de présentation de prestation sont toutes identiques dans leur structure. Chaque prestation possède un ou plusieurs aspects qui sont décrits d'abord en globalité pour expliquer le processus de fabrication, puis en détails à travers des images des réalisations liées à cette prestation et un texte descriptif des étapes de conception du produit ou de réalisation du service.</p>
+            <div class="flex flex-col items-center gap-5 mb-8 italic desktop:text-lg text-base">
+                <img class="mt-6 w-full desktop:w-[1000px]" src="/img/jhm/jhm-presta.png" alt="Page de présentation de l'entreprise">
+                <caption>Exemple d'une page de présentation des prestations de l'entreprise</caption>
+            </div>
+            <p>Malheureusement, ces pages ne sont pas encore complètes. En effet, hormis les illustrations les pages de présentation des prestations font partie des pages où les contenus textuels n'ont pas été fournis. En conséquences, les textes ont été provisoirement remplacés par du Lorem Ipsum.</p>
+        </div>
+        <!--Maquette-->
+        <div class="mb-8 desktop:mx-[428px]">
+            <h3 class="font-gothic font-medium text-xl desktop:text-3xl my-6" id="maquette">Maquette</h3>
+            <div class="flex flex-col items-center">
+                <img class="my-6 w-full desktop:w-[1000px]" src="/img/jhm/m_jhm.png" alt="Maquette JHM CONCEPT">
+                <a href="https://www.figma.com/file/bxCBesMgBdcXGnbNEz5tDw/JHM-CONCEPT?type=design&node-id=1%3A5&mode=design&t=j6Bc2BgdsciBrMeD-1" target="_blank">
+                    <button class="font-cabin rounded-full shadow shadow-black/60 bg-orange-500 py-2 px-6 desktop:py-4 desktop:px-12 uppercase text-lg desktop:text-2xl font-black mb-8">
+                        Voir la maquette
+                    </button>
+                </a>
+            </div>
+            <p>Pour être sûr de correspondre aux attentes de l'entreprise, j'ai réalisé un prototype sous forme de maquette à l'aide de Figma, en commençant par définir l'identité visuelle de notre site, à l'aide d'un guide de style.</p>
+            <div class="flex flex-col items-center gap-5 mb-8 italic desktop:text-lg text-base">
+                <img class="mt-6 w-full desktop:w-[1000px]" src="/img/jhm/m_jhm-guide.png" alt="Guide de style maquette JHM CONCEPT">
+                <caption>Guide de style de la maquette</caption>
+            </div>
+            <p>Grâce à ce guide de style, j'ai pu mettre en place les bases du site telles que les typographies utilisées, les composants comme les carrousels et la conception de l'harmonie colorée du site. C'est en m'aidant de ces données que j'ai pu réaliser une maquette complète et fonctionnelle.</p>
+            <div class="flex flex-col items-center gap-5 mb-8 italic desktop:text-lg text-base">
+                <img class="mt-6 w-full desktop:w-[500px]" src="/img/jhm/m_jhm-phone.png" alt="Maquette format mobile JHM CONCEPT">
+                <caption>Maquette du site Velotaf NFC</caption>
+            </div>
+            <p>Le site ayant pour but d'être responsive, j'ai entrepris de réaliser deux maquettes distinctes, une pour ordinateur et une pour téléphone (au format Iphone X). On peut également constater quelques différences entre la maquette et le site actuel, notamment l'absence de 2 pages qui ont été réalisées après avoir terminé la maquette, ou au niveau de l'apparence des carrousels, qui n'ont pas pu être reproduits tels quels lors de l'intégration sur WordPress.</p>
+            <p>Malgré ces inconvénients, le site complet de 21 pages à réussi à correspondre et répondre adéquatement aux besoins de l'entreprise.</p>
+        </div>
+        <!--Visuels-->
+        <div class="mb-8 desktop:mx-[428px]">
+            <h3 class="font-gothic font-medium text-xl desktop:text-3xl my-6" id="visuels">Visuels</h3>
+            <div class="flex flex-col items-center">
+                <img class="my-6 w-full desktop:w-[500px]" src="/img/jhm/v_jhm-recto.png" alt="Visuels maquette Velotaf NFC">
+                <a href="https://www.figma.com/file/bxCBesMgBdcXGnbNEz5tDw/JHM-CONCEPT?type=design&node-id=399%3A1548&mode=design&t=62TOZsR4GTLgFAqX-1" target="_blank">
+                    <button class="font-cabin rounded-full shadow shadow-black/60 bg-orange-500 py-2 px-6 desktop:py-4 desktop:px-12 uppercase text-lg desktop:text-2xl font-black mb-8">
+                        Voir les visuels
+                    </button>
+                </a>
+            </div>
+            <p>Lors de mon stage, j'ai également été chargé de réaliser un template de carte de visite pour JHM CONCEPT, à réaliser en parallèle du développement du site web. Dans mon processus de création, je suis passé par plusieurs visuels différents (sans compter les variantes de ces-dits visuels) avant d'atteindre un modèle qui convenait à l'équipe de l'entreprise.</p>
+            <div class="flex flex-col items-center gap-5 mb-8 italic desktop:text-lg text-base">
+                <img class="mt-6 w-full desktop:w-[500px]" src="/img/jhm/v_jhm-essais1.png" alt="Premiers essais carte de visite JHM CONCEPT">
+                <caption>Premiers essais de visuels pour la carte de visite</caption>
+                <img class="mt-2 w-full desktop:w-[500px]" src="/img/jhm/v_jhm-essais2.png" alt="Deuxième phase recherches carte de visite JHM CONCEPT">
+                <caption>Deuxième phase de recherches pour la carte de visite</caption>
+                <img class="mt-2 w-full desktop:w-[500px]" src="/img/jhm/v_jhm-essais3.png" alt="Phase finale recherches carte de visite JHM CONCEPT">
+                <caption>Phase finale de recherches pour la carte de visite</caption>
+            </div>
+            <p>Durant la première phase de recherche, je me suis inspiré de plusieurs templates de cartes existants, en intégrant des éléments graphiques qui pourraient rappeler le coeur de métier de l'entreprise (la vis, la règle, etc.)</p>
+            <p class="mt-8">Pour la seconde phase, j'ai pris en compte les remarques des membres de l'entreprise et j'ai commencé à rechercher un visuel qui puisse mieux permettre aux éléments de s'imbriquer les uns aux autres.</p>
+            <p class="mt-8">Lors de la dernière phase, j'ai repris ce concept en modifiant encore une fois en fonction des remarques de l'équipe et j'ai également recherché quelque chose de plus sobre. C'est avec cet objectif en tête que je suis parvenu à livrer un template final qui correspondait aux attentes de l'entreprise. J'ai également réalisé les versos, en deux versions de couleurs différentes (et en adaptant donc le logo de l'entreprise à chaque version).</p>
+            <div class="flex flex-col items-center gap-5 mb-8 italic desktop:text-lg text-base">
+                <img class="mt-6 w-full desktop:w-[500px]" src="/img/jhm/v_jhm-recto.png" alt="Premiers essais carte de visite JHM CONCEPT">
+                <caption>Version finale du modèle de carte de visite (recto)</caption>
+                <img class="mt-2 w-full desktop:w-[500px]" src="/img/jhm/v_jhm-versob.png" alt="Premiers essais carte de visite JHM CONCEPT">
+                <caption>Version finale du modèle de carte de visite (verso blanc)</caption>
+                <img class="mt-2 w-full desktop:w-[500px]" src="/img/jhm/v_jhm-verson.png" alt="Premiers essais carte de visite JHM CONCEPT">
+                <caption>Version finale du modèle de carte de visite (verso noir)</caption>
+            </div>
+            <div class="flex flex-col desktop:flex-row place-content-center gap-8 mx-8 my-6">
+                <button class="rounded-full shadow shadow-black/60 border-orange-500 border-2 bg-blanc-gris/30 py-2 px-6 uppercase text-lg font-black" @click="downloadPdf">Télécharger la carte de visite</button>
+            </div>
+        </div>
+        <!--Contact-->
+        <div class="text-blanc-gris flex flex-col items-center mb-16">
+            <h2 class="font-gothic text-center text-lg mt-8 mb-6 desktop:text-3xl desktop:mt-16">Vous souhaitez me contacter ?</h2>
+            <a href="/contact">
+                <button class="font-cabin rounded-full shadow shadow-black/60 bg-orange-500 py-2 px-6 desktop:py-4 desktop:px-12 uppercase text-lg desktop:text-2xl font-black">
+                    Contact
+                </button>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        downloadPdf() {
+            fetch('/src/assets/pdf/jhm_carte-visite.pdf')
+            .then(response => response.blob())
+            .then(blob => {
+            const url = URL.createObjectURL(blob);
+            window.open(url);
+            })
+            .catch(error => console.error(error));
+        },
+    },
+}
+</script>
