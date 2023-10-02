af373b7b55c2b6e5fa6c8472dc99a91131bb96e2
monturet-matthieu <monturetmatthieu@gmail.com>
2023-10-02 09:23:04 +0200

    maj CV + dl pdf about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -168,13 +168,13 @@ import ProgressBar from '../components/ProgressBar.vue';
 export default {
     methods: {
         downloadPdf() {
-            fetch("../../public/pdf/CV_Matthieu_Monturet.pdf")
-                .then(response => response.blob())
-                .then(blob => {
-                const url = URL.createObjectURL(blob);
-                window.open(url);
-            })
-                .catch(error => console.error(error));
+            const defaultPdfUrl = '/pdf/CV_Matthieu_Monturet.pdf';
+            const pdfUrl = this.pdfLink || defaultPdfUrl;
+            const newWindow = window.open(pdfUrl, '_blank');
+
+            if (!newWindow || newWindow.closed || typeof newWindow.closed == 'undefined') {
+            console.error("Le navigateur a bloqué l'ouverture de la nouvelle fenêtre. Veuillez autoriser les pop-ups pour ce site.");
+            }
         },
     },
     components: { ProgressBar }
